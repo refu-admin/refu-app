4ed5a894d538bf2876fefde7d012ceb8e732ff11
refu-admin <92260225+refu-admin@users.noreply.github.com>
2022-02-04 13:12:59 +0900

    ログアウトURLの訂正

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,11 +7,11 @@ app_name='user_auth'
 
 urlpatterns=[
     # path('index/', views.index, name='index'),
-    path('top/',views.top_page, name="top"), # リダイレクト
-    path('login/', # ログイン
+     path('top/',views.top_page, name="top"), # リダイレクト
+     path('login/', # ログイン
      django.contrib.auth.views.LoginView.as_view(template_name = 'user_auth/login.html'),
      name='login'),
-path('logout/', # ログアウト
+     path('logout/', # ログアウト
      django.contrib.auth.views.LogoutView.as_view(template_name = 'user_auth/login.html'),
      name='logout'),
 ]
