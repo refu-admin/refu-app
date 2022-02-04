16ba021cbf0d495995386ee348c36a51e942901c
refu-admin <92260225+refu-admin@users.noreply.github.com>
2022-02-04 15:20:24 +0900

    loginボタン変更

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name='user_auth'
 
 urlpatterns=[
-    # path('index/', views.index, name='index'),
+     path('',views.top_page, name = "default"),
      path('top/',views.top_page, name="top"), # リダイレクト
      path('login/', # ログイン
      django.contrib.auth.views.LoginView.as_view(template_name = 'user_auth/login.html'),
